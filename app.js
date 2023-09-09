9750c5654530fe3b6a19b55689646d8ddccf79f2
isotop786 <marufislam77@gmail.com>
2023-09-09 13:22:44 +0600

    db init

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express();
-const dotenv = require('dotenv').config()
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-app.get('/',(req,res)=>{
-    res.send("it's working!")
+// import routes
+const userRoutes = require('./routes/user.route')
+
+// db
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.DATABASE);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
+// routes middleware
+app.use("/api/users",userRoutes)
+
+
+connectDB().then(()=>{
+    app.listen(process.env.PORT ,()=>{
+        console.log("Node.js backend running on: http://localhost:"+process.env.PORT)
+    })
 })
 
 
-app.listen(process.env.PORT,()=>{
-    console.log("Node.js backend running on: http://localhost:"+process.env.PORT)
-})
